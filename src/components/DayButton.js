9dc6deccb6d1d28346d53cfed0742d337acc3d38
adr29truck <adrianalmetunsmeds@gmail.com>
2021-01-09 15:12:33 +0100

    :tada: DayButton

---
+++ src/components/DayButton.js
@@ -0,0 +1,23 @@
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+export const DayButton = (props) => {
+  // props
+  // object - obj {id, isChecked, style, name}
+  // onClickCheckBox - Func
+
+  return (
+  <FormControlLabel
+    control={
+      <Checkbox
+        style={ props.object.style }
+        name={props.object.name}
+        // checked={props.object.isChecked}
+        color="primary"
+        id={toString(props.object.id)}
+        onChange={props.onClickCheckBox}
+      />
+    }
+    label={ props.object.id < 10 ? '0' + props.object.id : props.object.id }
+  />)
+}
