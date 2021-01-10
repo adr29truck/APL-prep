ab138d6551978defa6b901369e76bb8a3ef82086
adr29truck <adrianalmetunsmeds@gmail.com>
2021-01-10 13:18:54 +0100

    :hammer: fixed linting errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 // import logo from './logo.svg';
-import PropTypes from 'prop-types';
 import React, {useState} from 'react';
 
 import './App.css';
@@ -9,11 +8,20 @@ import {DayButtonList} from './components/DayButtonList';
 import {ActivityDropDown} from './components/ActivityDropDown';
 import API from './helpers/API';
 
+
+/**
+ * Default app handler
+ * @return {jsx} - App render
+ */
 export function App() {
   const [times, setTimes] = useState([]);
   const [activitiy, setActivity] = useState(0);
   const [submitted, setSubmitted] = useState(false);
 
+  /**
+   * Submitts changes to times to the backend
+   * @param {*} _ - Uninportant
+   */
   async function onClick(_) {
     const filteredTimes = times.filter((x) => x.isChecked === true);
     for (const t of filteredTimes) {
@@ -24,6 +32,10 @@ export function App() {
 
   const [day, setDay] = useState(new Date());
 
+  /**
+   * Returns the selected day as a date string
+   * @return {string} - day objects date as string
+   */
   function getDayAsString() {
     let temp = day;
     if (temp.getTimezoneOffset() < 0) {
@@ -34,14 +46,22 @@ export function App() {
     return temp.toISOString().split('T')[0];
   }
 
+  /**
+   * Updates day when going backwards in time
+   */
   function onClickLeftArrow() {
     setDay((q) => new Date(q.valueOf() - 86_400_000));
   }
 
+  /**
+   * Updates day when going forward in time
+   */
   function onClickRightArrow() {
     const temp = new Date(day + 86_400_000);
     const temp2 = new Date();
-    if (!(temp.getFullYear() === temp2.getFullYear() && temp.getDate() === temp2.getDate() && temp.getMonth() === temp2.getMonth())) {
+    if (!(temp.getFullYear() === temp2.getFullYear() &&
+        temp.getDate() === temp2.getDate() &&
+        temp.getMonth() === temp2.getMonth())) {
       setDay((q) => new Date(q.valueOf() + 86_400_000));
     }
   }
