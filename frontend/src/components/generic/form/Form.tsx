78f7280a885d1d349b3bf8deb95c67c2b5900691
adr29truck <adrianalmetunsmeds@gmail.com>
2021-01-22 15:01:32 +0100

    :hammer: Fixing #5

--- frontend/src/components/generic/form/Form.tsx
+++ frontend/src/components/generic/form/Form.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/require-default-props */
-import { capitalize } from '@material-ui/core';
+import { TextField, capitalize, InputLabel } from '@material-ui/core';
 import React from 'react';
 
 interface X {
@@ -17,14 +17,18 @@ interface Props {
 
 const Form = ({ fields, onSubmit }: Props) => {
   const fieldsJsx = fields.map((field) => (
-    <input
-      type={field.type}
-      placeholder={field.placeholder || capitalize(field.name)}
+    <div>
+      <InputLabel htmlFor={`input_field_${field.name}`}>{capitalize(field.name)}</InputLabel>
+      <TextField
+      id={`input_field_${field.name}`}
       name={field.name}
-      value={field.value}
       onChange={field.onChange}
-      key={field.name}
-    />
+      placeholder={field.placeholder || capitalize(field.name)}
+      type={field.type}
+      value={field.value}
+      inputProps={{ 'aria-label': field.placeholder }}
+      />
+    </div>
   ));
 
   return (
