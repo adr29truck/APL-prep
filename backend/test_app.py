2a6c42cc4c84f6bec15963b08b9db85243a8c63b
adr29truck <adrianalmetunsmeds@gmail.com>
2021-01-27 14:18:07 +0100

    Testing activity retrival / Generation

--- backend/test_app.py
+++ backend/test_app.py
@@ -31,3 +31,13 @@ def test_login(client):
     x = json.loads(rv.data)
     header["Authorization"] = x["access_token"]
     assert header["Authorization"] is not None
+
+
+def test_activity_fetch(client):
+    """Validates activity retrival"""
+    rv = client.get(
+        "/times/2021-01-02", headers = header)
+    x = json.loads(rv.data)
+    assert x[0] == {"activity_id": None, 'color': None, 'id': 1, 'name': '2021-01-02T0', 'user_id': 1}
+    assert x[12] == {"activity_id": None, 'color': None, 'id': 13, 'name': '2021-01-02T12', 'user_id': 1}
+    assert x[23] == {"activity_id": None, 'color': None, 'id': 24, 'name': '2021-01-02T23', 'user_id': 1}
