f175461bd45750f4a0c841a4ce008805640fee14
adr29truck <adrianalmetunsmeds@gmail.com>
2021-01-29 09:12:57 +0100

    ENV preperation for production

--- backend/const.py
+++ backend/const.py
@@ -1,5 +1,10 @@
 import os
 
-DB_CONNECTION_STRING = (
-    f"postgresql+psycopg2://postgres:docker@{os.environ.get('host')}:5432/postgres"
-)
+if os.environ.get('ENV') != 'production':
+    DB_CONNECTION_STRING = (
+        f"postgresql+psycopg2://postgres:docker@{os.environ.get('host')}:5432/postgres"
+    )
+else:
+    DB_CONNECTION_STRING = (
+        f"postgresql+psycopg2://{os.environ.get('DATABASE_URL')}"
+    )
