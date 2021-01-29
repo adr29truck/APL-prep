f92ad85511eb0cf5ba81eda3675ea44813f65127
adr29truck <adrianalmetunsmeds@gmail.com>
2021-01-29 10:04:39 +0100

    Linted

--- backend/const.py
+++ backend/const.py
@@ -1,10 +1,8 @@
 import os
 
-if os.environ.get('ENV') != 'production':
+if os.environ.get("ENV") != "production":
     DB_CONNECTION_STRING = (
         f"postgresql+psycopg2://postgres:docker@{os.environ.get('host')}:5432/postgres"
     )
 else:
-    DB_CONNECTION_STRING = (
-        f"postgresql+psycopg2://{os.environ.get('DATABASE_URL')}"
-    )
+    DB_CONNECTION_STRING = f"postgresql+psycopg2://{os.environ.get('DATABASE_URL')}"
