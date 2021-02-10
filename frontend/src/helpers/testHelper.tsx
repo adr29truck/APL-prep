171157bc46823d9f16504e2124b230ceaf2b0356
adr29truck <adrianalmetunsmeds@gmail.com>
2021-02-10 14:14:02 +0100

    :tada: Support for react-router in tests

--- frontend/src/helpers/testHelper.tsx
+++ frontend/src/helpers/testHelper.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from '../store';
 
 export const TestHelper = ({ children }: any) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <BrowserRouter>
+      <Provider store={store}>{children}</Provider>
+    </BrowserRouter>
+  );
 };
