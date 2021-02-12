c12fdd39b1c4db1e6d2d0e68bcacdffb40ee700b
adr29truck <adrianalmetunsmeds@gmail.com>
2021-02-12 10:11:04 +0100

    Alembic env should get the connection url from const.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import const
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -37,7 +39,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = const.DB_CONNECTION_STRING
     context.configure(
         url=url,
         target_metadata=target_metadata,
